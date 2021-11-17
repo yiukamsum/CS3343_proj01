fea6b198b476ed972def18f921235e9e83fe7fd2
chunmwan4 <90747842+chunmwan4@users.noreply.github.com>
2021-11-17 21:16:53 +0800

    Update and rename TicketElderly.java to Elderly.java

---
+++ src/obj/Elderly.java
@@ -0,0 +1,13 @@
+package obj;
+
+public class Elderly implements Ticket {
+
+  public double getPrice(){
+    return 100.00 * 0.7;
+  }
+  
+  public String getType(){
+    return "Elderly";
+  }
+  
+}
--- src/obj/TicketElderly.java
+++
@@ -1,5 +0,0 @@
-package obj;
-
-public class TicketElderly extends Ticket {
-
-}
