1a549aecea65ce5ca48d99b2f8b2dfcd17c79640
chunmwan4 <90747842+chunmwan4@users.noreply.github.com>
2021-11-17 21:17:46 +0800

    Update Adult.java

--- src/obj/Adult.java
+++ src/obj/Adult.java
@@ -2,11 +2,11 @@ package obj;
 
 public class Adult implements TicketType {
     public double getPrice(){
-         return 100.00;
+      return 100.00;
     }
   
     public String getType(){
-         return "Adult";
+      return "Adult";
     }
   
 }
