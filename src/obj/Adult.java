3a25fe3ed2658a4d95a31e991e35f01685135d35
chunmwan4 <90747842+chunmwan4@users.noreply.github.com>
2021-11-17 21:27:32 +0800

    Update Adult.java

--- src/obj/Adult.java
+++ src/obj/Adult.java
@@ -1,10 +1,13 @@
 package obj;
 
 public class Adult implements TicketType {
+    
+    @Override
     public double getPrice(){
       return 100.00;
     }
   
+    @Override
     public String getType(){
       return "Adult";
     }
