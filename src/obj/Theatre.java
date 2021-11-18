d021af7bd149d49d77cbbf0717ec446ed2ead8bf
chunmwan4 <90747842+chunmwan4@users.noreply.github.com>
2021-11-18 10:21:09 +0800

    Update Theatre.java

--- src/obj/Theatre.java
+++ src/obj/Theatre.java
@@ -1,14 +1,32 @@
 package obj;
 
-import java.util.ArrayList;
+import java.util.*;
 
 public class Theatre {
-	private String cinemaId;
 	private String theatreId;
-	private Integer totalCapacity;
-	private ArrayList<ShowingSession> sessionOnShow;
+	private Integer colNum;
+	private Integer rowNum;
 	
-	public String method(String type) {
-		return type;
+	public Theatre(String theatreId, Integer colNum, Integer rowNum ) {
+		this.theatreID = theatreID;
+		this.colNum = colNum;
+		this.rowNum = rowNum;
 	}
+	
+	public Integer getColNum(){
+		return colNum;
+	}
+	
+	public Integer getRowNum(){
+		return rowNum;
+	}
+	
+	public String getTheatreID(){
+		return theatreID;
+	}
+	
+	public Integer compareTo(Theatre theatre){
+		return this.theatreID.compareTo(theatre.theatreID);
+	}
+	
 }
