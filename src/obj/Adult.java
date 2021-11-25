5a3132eb4fa50511992f8414e54f470ee2554531
Fungc97 <90034836+Fungc97@users.noreply.github.com>
2021-11-25 11:12:49 +0800

    update singleton classes

--- src/obj/Adult.java
+++ src/obj/Adult.java
@@ -1,7 +1,16 @@
 package obj;
 
 public class Adult implements TicketType {
-    
+	private static Adult instance=new Adult();
+	
+	
+	private Adult() {
+		
+	}
+	public Adult getInstance() {
+		return instance;
+	}
+	
     public double getPrice(){
       return 100.00;
     }
--- src/obj/Child.java
+++ src/obj/Child.java
@@ -1,7 +1,16 @@
 package obj;
 
 public class Child implements TicketType {
-
+	private static Child instance=new Child();
+	
+	
+	private Child() {
+		
+	}
+	public Child getInstance() {
+		return instance;
+	}
+	
   public double getPrice(){
      return 100.00 * 0.75;
    }
