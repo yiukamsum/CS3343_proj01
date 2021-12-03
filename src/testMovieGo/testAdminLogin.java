3c3be6844d15834d6b5c800b7297cfa5bc06b399
plasticny <2220354896@qq.com>
2021-12-03 16:54:25 +0800

    update member and admin login testing

--- src/testMovieGo/testAdminLogin.java
+++ src/testMovieGo/testAdminLogin.java
@@ -8,11 +8,18 @@ import org.junit.jupiter.api.Test;
 
 import obj.Admin;
 import obj.AdminLogin;
+import obj.DbAction;
 import obj.UserConsole;
 
 public class testAdminLogin {
     @Test
     public void testAdminLogin() {
+        class getAdminAction extends DbAction {
+            public Admin getAdmin() {
+                return getDatabase().getAdmin("Jack", "123");
+            }
+        }
+
         String input="Jack\n123\n";
 		System.setIn(new ByteArrayInputStream(input.getBytes()));
 
@@ -20,7 +27,8 @@ public class testAdminLogin {
         AdminLogin adminLogin = new AdminLogin();
         
         Admin actual = (Admin)adminLogin.login(console);
+        Admin expected = new getAdminAction().getAdmin();
 
-        assertEquals("Jack", actual.getAccountName());
+        assertEquals(expected, actual);
     }
 }
--- src/testMovieGo/testMemberLogin.java
+++ src/testMovieGo/testMemberLogin.java
@@ -8,11 +8,18 @@ import org.junit.jupiter.api.Test;
 
 import obj.MemberLogin;
 import obj.UserConsole;
+import obj.DbAction;
 import obj.Member;
 
 public class testMemberLogin {
     @Test
     public void testMemberLogin() {
+        class getMemberAction extends DbAction {
+            public Member getMember() {
+                return getDatabase().getMember("Peter", "123");
+            }
+        }
+
         String input="Peter\n123\n";
 		System.setIn(new ByteArrayInputStream(input.getBytes()));
 
@@ -20,7 +27,8 @@ public class testMemberLogin {
         MemberLogin memberLogin = new MemberLogin();
         
         Member actual = (Member)memberLogin.login(console);
+        Member expected = new getMemberAction().getMember();
 
-        assertEquals("Peter", actual.getAccountName());
+        assertEquals(expected, actual);
     }
 }
